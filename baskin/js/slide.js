0902cb5395d8e0a4f9024d55b8ce24558ca36fc8
leede <cjsakfn2@gmail.com>
2021-11-29 21:39:34 +0900

    html,css완성

--- baskin/js/slide.js
+++ baskin/js/slide.js
@@ -4,12 +4,13 @@ carousel();
 
 function carousel() {
     var i;
-    var x = document.getElementsByClassName("slideitem");
+    var x = document.getElementsByClassName("imgslide4");
     for (i = 0; i < x.length; i++) {
-       x[i].style.display = "none";  
+        x[i].style.display = "none";  
     }
     myIndex++;
     if (myIndex > x.length) {myIndex = 1}    
     x[myIndex-1].style.display = "block";  
     setTimeout(carousel, 3000); // Change image every 2 seconds
 }
+
